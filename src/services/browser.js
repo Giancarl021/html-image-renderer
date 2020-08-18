888df8ab986dfc7cc63e5a5ac798945b796eb4bd
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-08-18 00:52:01 -0300

    Initial files

---
+++ src/services/browser.js
@@ -0,0 +1,62 @@
+const puppeteer = require('puppeteer');
+const locate = require('./locate');
+
+module.exports = function () {
+    let browser, page;
+
+    async function start(options = {}) {
+        if (browser) return;
+        browser = await puppeteer.launch(options);
+    }
+
+    async function close() {
+        if (page) await page.close();
+        if (browser) await browser.close();
+        page = browser = null;
+    }
+
+    async function goTo(url) {
+        page = await browser.newPage();
+        await page.goto(url);
+
+    }
+
+    async function exec(functionName, ...args) {
+        await eval((fn, ...args) => {
+            window[fn](...args);
+        }, functionName, ...args);
+    }
+
+    async function eval(fn, ...args) {
+        await page.evaluate(fn, ...args);
+    }
+
+    async function screenshot(dest, width = null, height = null, returnAsBuffer = false) {
+        if (width && height) {
+            await page.setViewport({
+                width,
+                height
+            });
+        } else if (width) {
+            await page.setViewport({
+                width,
+                height: width
+            });
+        }
+
+        if (returnAsBuffer) return await page.screenshot();
+
+        await page.screenshot({
+            path: locate(dest)
+        });
+    }
+
+    return {
+        start,
+        goTo,
+        exec,
+        eval,
+        screenshot,
+        close
+    }
+}
