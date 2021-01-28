4f2ceb18845d42cee3cba3ab9a1130538c8998d6
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2021-01-28 17:08:23 -0300

    Fixed locate.js

--- src/util/locate.js
+++ src/util/locate.js
@@ -5,5 +5,5 @@ module.exports = function(pathToFile, useCWD = false) {
     if(!pathToFile) throw new Error('Empty path');
     const _path = pathToFile.replace('~', homedir()).replace(/\\/g, '/');
     if(path.isAbsolute(_path)) return _path;
-    return (useCWD ? path.resolve(process.cwd(), _path) : path.resolve(__dirname, '..', '..', _path)).replace(/\\/g, '/');
+    return (useCWD ? path.resolve(process.cwd(), _path) : path.resolve(path.dirname(require.main.filename), _path)).replace(/\\/g, '/');
 };
